7bcb010a567cc7312e545a4c4d933cf44a0daeb8
Admir Cosic <222551@skane.se>
2021-01-16 11:33:36 +0100

    styling and content changes

--- src/root.component.js
+++ src/root.component.js
@@ -1,5 +1,10 @@
 import React from "react";
+import "./root.component.css";
 
-export default function Root(props) {
-  return <section>{props.name} is mounted!</section>;
+export default function Root() {
+  return (
+    <div className="app2-container">
+      <p>App2 is alive and kicking</p>
+    </div>
+  );
 }
